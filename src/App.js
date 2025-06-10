519e3f6b894b0a414c71dd225fb0375de36a2062
sindhu <muchalla.sindhura@gmail.com>
2025-06-09 21:42:30 -0500

    some new changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         HI this is sindhu! welcome to my new learnings
+         HI this is sindhu! welcome to my new learnings. still learnings
         </p>
         <a
           className="App-link"
