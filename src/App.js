4a949f2a355593f37eeabc78d2d7958cae90ed08
Reddy0724 <muchalla.sindhura@gmail.com>
2025-06-09 21:42:07 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         HI this is sindhu! welcome to my new learnings
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
